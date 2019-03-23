1842b78c0d121dc6d992bf6bd8e79006cdd5fef0
VidhiRambhia <vsrambhia@gmail.com>
2019-03-23 10:45:36 +0530

    Unnecessary things from forms removed'

--- synergyMain/routes.py
+++ synergyMain/routes.py
@@ -4,7 +4,7 @@ from synergyMain import app, db
 #from synergyMain.globalVar import shortlist
 from PIL import Image
 from flask import Flask, session, escape, render_template, url_for, flash, redirect, request
-from synergyMain.forms import LoginForm, SelectForm
+from synergyMain.forms import LoginForm, SelectForm, UpdateAccountForm
 from synergyMain.models import User, Conversing, Conversation
 import hashlib #for SHA512
 from flask_login import login_user, current_user, logout_user, login_required
@@ -39,7 +39,7 @@ def register():
                 db.session.add(user)
                 db.session.commit()
                 flash(f'Success! Please fill in the remaining details', 'success')
-            return redirect(url_for('registerSponsor'))
+            return redirect(url_for('login'))
     else: print('halaaaa')
     return render_template('reg.html', form=form)
 
